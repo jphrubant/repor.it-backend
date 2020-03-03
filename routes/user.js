6a685fbb456b84b3e15f711b6d8bd8ed26114432
jphrubant <jphrubant@gmail.com>
2020-03-03 20:00:07 +0100

    update readme

--- routes/user.js
+++ routes/user.js
@@ -3,41 +3,6 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const User = require("../models/user-model");
 
-// GET ONE USER //
-router.get('/:id', (req, res, next) => {
-    const {id} = req.params;
-    User
-        .findById(id)
-        .populate('reports')
-        .then(oneUser => {
-            res
-              .status(200)
-              .json(oneUser);
-        })
-        .catch(err => {
-            res
-            .status(400)
-            .json(err); 
-        });
-});
-
-// CREATE A USER//
-router.post('/', (req, res, next) => {
-    const {email, password, dateOfBirth, sex, sexualOrientation, ethnicity, nationality} = req.body;
-    User
-        .create({email, password, dateOfBirth, sex, sexualOrientation, ethnicity, nationality, reports:[]})
-        .then(newUser => {
-            res
-              .status(201)
-              .json(newUser); 
-        })
-        .catch(err => {
-            res
-            .status(500)
-            .json(err); 
-        });
-});
-
 // UPDATE A USER //
 router.put('/:id', (req, res, next) => {
     const {id} = req.params;
