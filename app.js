9e0b36b02afc7697eb3f723abe1736e61809dc8a
jphrubant <jphrubant@gmail.com>
2020-03-09 16:26:43 +0100

    Production deployment v1

--- app.js
+++ app.js
@@ -33,7 +33,9 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN],
+    origin: [process.env.PUBLIC_DOMAIN, 
+            "https://report-it-web-app.herokuapp.com"
+    ]
   }),
 );
 // app.use((req, res, next) => {
@@ -72,6 +74,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/auth', auth);
 app.use('/report', report);
 app.use('/user', user);
+
+// ROUTE FOR SERVING REACT APP (index.html)
+app.use((req, res, next) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // 404 
 // catch 404 and forward to error handler
---
+++ public/precache-manifest.ba4bfb1470bf42522f583fc52dec7d88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2024b15da09e47b4939f5c5cee3f33c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c79bc287d5ab6151f38b",
+    "url": "/static/css/main.ed0aeee9.chunk.css"
+  },
+  {
+    "revision": "fcae0e2ae896ffb82900",
+    "url": "/static/js/2.e38244b9.chunk.js"
+  },
+  {
+    "revision": "278573b4c188b9052aff83eaf7bb2ca3",
+    "url": "/static/js/2.e38244b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79bc287d5ab6151f38b",
+    "url": "/static/js/main.fc8205aa.chunk.js"
+  },
+  {
+    "revision": "5dde3ecc687698d22105",
+    "url": "/static/js/runtime-main.22b866d5.js"
+  }
+]);
